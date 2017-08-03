57a4b5912ffd1f8c94e4405cd87be42b38bce216
mizow8 <mizo.w8@hotmail.com>
2017-08-03 18:13:02 +0800

    initial implementation of heapster+influxdb+grafana

--- monitoring/k8s/controller/run.sh
+++ monitoring/k8s/controller/run.sh
@@ -2,30 +2,28 @@
 
 set -e
 
-MASTER_DNS=$1
-RESOURCE_LOCATION=$2
-MASTER_USERNAME=$3
-NGINX_PASSWORD=$4
-BASED_PRIVATE_KEY=$5
-REGISTRY_NAME=$6
-REGISTRY_PASS=$7
+RESOURCE_LOCATION=$1
+ADMIN_USERNAME=$2
+ADMIN_PASSWORD=$3
+MASTER_DNS=$4
+MASTER_USERNAME=$5
+BASED_PRIVATE_KEY=$6
 
-export REGISTRY_URL=${REGISTRY_NAME}.azurecr.io
-export STORAGE_ACCOUNT=$8
+export STORAGE_ACCOUNT=$7
 export STORAGE_LOCATION=${RESOURCE_LOCATION}
 
 PRIVATE_KEY='private_key'
 
-MASTER_URL=${MASTER_DNS}.${RESOURCE_LOCATION}.cloudapp.azure.com
+MASTER_URL=${MASTER_DNS}
 
 export KUBECONFIG=/root/.kube/config
 
 # prerequisite
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-apt-cache policy docker-ce
-sudo apt-get install -y unzip docker-ce nginx apache2-utils
+curl -fsSL https://mirror.azure.cn/docker-engine/apt/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://mirror.azure.cn/docker-engine/apt/repo ubuntu-xenial main"
+sudo apt-get update --fix-missing
+apt-cache policy docker-engine
+sudo apt-get install -y unzip docker-engine nginx apache2-utils
 
 # install kubectl
 cd /tmp
@@ -45,21 +43,22 @@ kubectl get nodes
 curl -s https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 helm init
 
+# !!temp workaround -- update tiller image pulling from mirror!!
+kubectl --namespace=kube-system set image deployments/tiller-deploy tiller=mirror.azure.cn:5000/kubernetes-helm/tiller:v2.5.1
+
 # download templates
-REPO_URL='https://github.com/Microsoft/elk-acs-kubernetes/archive/rc.zip'
+REPO_URL='https://miaosrc.blob.core.windows.net/src/microservice-reference-architectures.zip'
 
 curl -L ${REPO_URL} -o template.zip
 unzip -o template.zip -d template
 
 # expose kubectl proxy
-cd template/elk-acs-kubernetes-rc/
-echo ${NGINX_PASSWORD} | htpasswd -c -i /etc/nginx/.htpasswd ${MASTER_USERNAME}
-cp config/nginx-site.conf /etc/nginx/sites-available/default
+cd template/microservice-reference-architectures/
+echo ${ADMIN_PASSWORD} | htpasswd -c -i /etc/nginx/.htpasswd ${ADMIN_USERNAME}
+cp monitoring/k8s/controller/nginx-config/nginx-site.conf /etc/nginx/sites-available/default
 nohup kubectl proxy --port=8080 &
 systemctl reload nginx
 
-# push image & helm install 
-cd docker
-bash push-images.sh ${REGISTRY_NAME} ${REGISTRY_PASS} ${MASTER_USERNAME} ${NGINX_PASSWORD}
-cd ../helm-charts
-bash start-elk.sh ${REGISTRY_NAME} ${REGISTRY_PASS}
+# helm install 
+cd monitoring/k8s/controller
+bash start-heapster-influxdb-grafana.sh
